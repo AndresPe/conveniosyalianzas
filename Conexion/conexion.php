5c0a63d4f1bde2e60fcd5af8f78da67fd3c53e1f
STM <stefa0826.14@gmail.com>
2020-10-30 01:21:37 -0500

    NADA

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -6,11 +6,6 @@ function pdo_connect_mysql()
       $DATABASE_USER = 'root';
       $DATABASE_PASS = '';
       $DATABASE_NAME = 'convenios';
-<<<<<<< HEAD
-      
-=======
-     
->>>>>>> c1b834252c4575cac98929b2c97b26a43efce383
       try {
             return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
       } catch (PDOException $exception) {
