000fd002d81f7c43324549c9b39b65c7cae9e34e
karoljcsp <73604875+karoljcsp@users.noreply.github.com>
2020-10-30 02:20:33 -0500

    Index

--- Vistas/ConveniosNac/index.php
+++ Vistas/ConveniosNac/index.php
@@ -11,7 +11,7 @@ include '../../Conexion/conexion.php';
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>$title</title>
+		<title> </title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
@@ -20,9 +20,9 @@ include '../../Conexion/conexion.php';
     	<div>
     		<h1>C&A</h1>
             <a href="index.php"><i class="fas fa-home"></i>Solicitudes</a>
-            <a href="Vistas/ConveniosNacionales/read.php"></i>Convenios Nacionales</a>
-            <a href=""><i class="fas fa-address-book"></i>Convenios Internacionales</a>
-    		<a href=""><i class="fas fa-address-book"></i>Intercambios Academicos</a>
+            <a href="read.php"></i>Convenios Nacionales</a>
+            <a href="../ConveniosInter/read.php"><i class="fas fa-address-book"></i>Convenios Internacionales</a>
+    		<a href="../Intercambios/read.php"><i class="fas fa-address-book"></i>Intercambios Academicos</a>
             <li class="nav-item">
                 <a class="nav-link" href="#"><i class="fa fa-user"></i> Login</a>
             </li>
